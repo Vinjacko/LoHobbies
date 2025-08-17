8a8f11aa8b2d89106e2b74ae4e274bc103249555
Vinjacko <vin.iacovone@gmail.com>
2025-08-17 13:24:03 +0200

    Refactor Comments component styles and structure; update MediaPage layout for better responsiveness

--- frontend/src/components/media/Comments.js
+++ frontend/src/components/media/Comments.js
@@ -6,15 +6,17 @@ const Comments = ({ comments }) => {
         <div className="comments-section">
             <h2>Commenti</h2>
             {comments && comments.length > 0 ? (
-                comments.map(comment => (
-                    <div key={comment._id} className="comment">
-                        <div className="comment-author">
-                            <img src={comment.user.profilePicture ? (comment.user.profilePicture.startsWith('http') ? comment.user.profilePicture : `${process.env.REACT_APP_API_URL}/${comment.user.profilePicture}`) : '/img/Actor_Placeholder.png'} alt={comment.user.username} />
-                            <span>{comment.user.name}</span>
+                <div className="comments-list">
+                    {comments.map(comment => (
+                        <div key={comment._id} className="comment">
+                            <div className="comment-author">
+                                <img src={comment.user.profilePicture ? (comment.user.profilePicture.startsWith('http') ? comment.user.profilePicture : `${process.env.REACT_APP_API_URL}/${comment.user.profilePicture}`) : '/img/Actor_Placeholder.png'} alt={comment.user.username} />
+                                <span>{comment.user.name}</span>
+                            </div>
+                            <p>{comment.content}</p>
                         </div>
-                        <p>{comment.content}</p>
-                    </div>
-                ))
+                    ))}
+                </div>
             ) : (
                 <p>Nessun commento per questo contenuto.</p>
             )}
