4d1b1715ca1a2ccc90800c6e01a1e62edc2c966f
ang-ost <angelo.ostello.w@gmail.com>
2025-08-17 20:06:16 +0200

    Refactor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/layout/Header';
