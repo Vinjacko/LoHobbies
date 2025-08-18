96936d069ab9c9eea0aea515faa452503e444e15
Vinjacko <vin.iacovone@gmail.com>
2025-08-18 10:06:35 +0200

    Revisione commenti

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// creazione di un'istanza di axios per facilitarne l'utilizzo
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000',
   withCredentials: true,
