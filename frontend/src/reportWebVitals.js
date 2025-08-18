9ce08d9fc5836753a09ace62415fb342e057fe02
Vinjacko <vin.iacovone@gmail.com>
2025-08-18 12:21:05 +0200

    Modifiche a reportWebVitals

--- frontend/src/reportWebVitals.js
+++ frontend/src/reportWebVitals.js
@@ -1,11 +1,11 @@
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+      getCLS(onPerfEntry);  //  misura la velocità di caricamento
+      getFID(onPerfEntry);  //  misura l'interattività
+      getFCP(onPerfEntry);  //  misura la stabilità visiva.
+      getLCP(onPerfEntry);  //  misura il momento in cui il primo contenuto (testo, immagine, ecc.) viene visualizzato sullo schermo
+      getTTFB(onPerfEntry); //  misura il tempo che intercorre tra la richiesta di una risorsa e l'arrivo del primo byte della risposta.
     });
   }
 };
