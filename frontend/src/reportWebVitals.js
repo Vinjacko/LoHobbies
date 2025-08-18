c5abe16a26a0b9641cfa737e09d858b173cfdc57
Vinjacko <vin.iacovone@gmail.com>
2025-08-18 12:49:23 +0200

    modifica commenti

--- frontend/src/reportWebVitals.js
+++ frontend/src/reportWebVitals.js
@@ -5,7 +5,7 @@ const reportWebVitals = onPerfEntry => {
       getFID(onPerfEntry);  //  misura l'interattività
       getFCP(onPerfEntry);  //  misura la stabilità visiva.
       getLCP(onPerfEntry);  //  misura il momento in cui il primo contenuto (testo, immagine, ecc.) viene visualizzato sullo schermo
-      getTTFB(onPerfEntry); //  misura il tempo che intercorre tra la richiesta di una risorsa e l'arrivo del primo byte della risposta.
+      getTTFB(onPerfEntry); //  misura il tempo che intercorre tra la richiesta di una risorsa e l'arrivo del primo byte della risposta
     });
   }
 };
