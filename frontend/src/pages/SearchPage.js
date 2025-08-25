b048a5c51034202c3d59ed93b94cdb2083ee10d3
ang-ost <angelo.ostello.w@gmail.com>
2025-08-25 16:39:04 +0200

    upd

--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useTransition } from 'react';
+import { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import axios from '../api/axios';
 import { useFilters } from '../context/FilterContext';
