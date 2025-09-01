a9e9430be72576f25650cb515af185b4b4276e53
ang-ost <angelo.ostello.w@gmail.com>
2025-09-01 15:39:44 +0200

    refactor: simplify filter conditions in autocompleteSearch and remove unnecessary comment in upload middleware

--- Backend/middleware/upload.js
+++ Backend/middleware/upload.js
@@ -1,7 +1,6 @@
 const multer = require('multer');
 const path = require('path');
 
-// crea un motore di archiviazione per salvare i file sul disco del server
 const storage = multer.diskStorage({
   destination: './public/uploads/',
   filename: function(req, file, callback){
